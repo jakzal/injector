ea15c953b531ea2706634072aa264a4cb929e6d3
Jakub Zalas <jakub@zalas.pl>
2018-07-06 12:07:59 +0100

    Fix cs issues

--- tests/Service/Fixtures/ChildServices.php
+++ tests/Service/Fixtures/ChildServices.php
@@ -5,15 +5,15 @@ namespace Zalas\Injector\Tests\Service\Fixtures;
 
 class ChildServices extends Services
 {
-    /**
-     * @var Service1Custom
-     */
-    private $service1;
 
     /**
      * @var Service2Custom
      */
     protected $service2;
+    /**
+     * @var Service1Custom
+     */
+    private $service1;
 
     public function getChildService1(): ?Service1Custom
     {
